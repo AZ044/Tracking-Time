178b132b1eb8bd6613af6171bd2c5e608cbff5ba
Hugo <abrinohugo@gmail.com>
2026-05-01 17:02:42 +0200

    [+] add Icon image saved in /icons

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonWriter;
 import com.sun.jna.platform.win32.*;
 import com.sun.jna.platform.win32.WinDef.HWND;
 import com.sun.jna.ptr.IntByReference;
@@ -5,6 +7,7 @@ import com.sun.jna.win32.StdCallLibrary;
 import com.sun.jna.win32.W32APIOptions;
 import com.sun.jna.Native;
 
+import java.awt.image.BufferedImage;
 import java.time.Duration;
 import java.time.LocalDate;
 
@@ -18,6 +21,7 @@ import java.util.Timer;
 
 import java.io.*;
 import java.util.logging.Handler;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.filechooser.FileSystemView;
 import com.sun.jna.platform.win32.Kernel32;
@@ -30,7 +34,7 @@ public class Main {
         }
 
     public static void processus() {
-        String old_window = get_processus();
+        String old_window = GetProcessus();
         String new_window = old_window;
 
         while (true) {
@@ -49,7 +53,7 @@ public class Main {
 
             while (new_window.equals(old_window)) {
                 old_window = new_window;
-                new_window = get_processus();
+                new_window = GetProcessus();
                 // recupere le dernier element si il y a -
 
                 try{
@@ -64,19 +68,17 @@ public class Main {
 
             Duration duration = Duration.between(heure_depart,heure_fin);
             System.out.printf("\n Temps : " + duration.toSeconds() + " Second\n");
-            old_window = get_processus();
+            old_window = GetProcessus();
             new_window = old_window;
 
-            System.out.printf("\n path : "+ get_path() + "\n");
-            geticon();
-
+            System.out.printf("\n path : "+ GetPath() + "\n");
+            GetIcon();
+            GetIconAndSave();
             System.out.printf("\n----------------------\n");
-
         }
-
     }
 
-    private static String get_processus() {
+    private static String GetProcessus() {
         int nMax_Count = 100;
         char[] window_char = new char[nMax_Count];
         // recupere l'adresse memoire
@@ -89,28 +91,67 @@ public class Main {
         return String.valueOf(window_char,0,window);
     }
 
+    public static String GetIconAndSave() {
+        //  Récupérer l'icône
+        String exePath = GetPath();
 
-    private static  String get_path() {
-       HWND hwnd = User32.INSTANCE.GetForegroundWindow();
+        // Générer un nom unique basé sur le nom de l'exe
+        String exeName = new File(exePath).getName().replace(".exe", "");
+        String iconPath = "icons/" + exeName + ".png";
+        Icon icon = FileSystemView.getFileSystemView()
+                .getSystemIcon(new File(exePath));
 
-       //recupere le pid du processus
+        if (new File(iconPath).exists()){
+            return iconPath;
+        }
+
+        // Convertir Icon -> BufferedImage
+        BufferedImage bufferedImage = new BufferedImage(
+                icon.getIconWidth(),
+                icon.getIconHeight(),
+                BufferedImage.TYPE_INT_ARGB
+        );
+        Graphics2D g2d = bufferedImage.createGraphics();
+        icon.paintIcon(null, g2d, 0, 0);
+        g2d.dispose();
+
+        // 4. Créer le dossier si nécessaire et sauvegarder
+        try {
+            new File("icons").mkdirs();
+            ImageIO.write(bufferedImage, "PNG", new File(iconPath));
+            System.out.println("Icône sauvegardée : " + iconPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        // 5. Retourner le chemin pour la DB
+        return iconPath;
+    }
+
+    private static  String GetPath() {
+      HWND hwnd = User32.INSTANCE.GetForegroundWindow();
+
+      // Recupere l'id du (pid) du processus
         IntByReference pid = new IntByReference();
         User32.INSTANCE.GetWindowThreadProcessId(hwnd,pid);
 
-        //ouvre le processus
-        WinNT.HANDLE hProcess = Kernel32.INSTANCE.OpenProcess(0x0400 | 0x0010, false,pid.getValue());
+      // ouvre le processus
+      WinNT.HANDLE hProcess = Kernel32.INSTANCE.OpenProcess(0x0400 | 0x0010,false,pid.getValue());
 
-        // obtenir le chemin complet
-        char [] path = new char[1024];
+      // Obtient le chemin du processus
+        char[] path = new char[1024];
         IntByReference size = new IntByReference(1024);
         Kernel32.INSTANCE.QueryFullProcessImageName(hProcess,0,path,size);
         Kernel32.INSTANCE.CloseHandle(hProcess);
 
         return Native.toString(path);
     }
-    public static void  geticon (){
+
+    public static void  GetIcon (){
         Icon icon = FileSystemView.getFileSystemView()
-                .getSystemIcon(new File(get_path()));
+                .getSystemIcon(new File(GetPath()));
+
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(new JLabel(icon));
@@ -118,5 +159,21 @@ public class Main {
         frame.setVisible(true);
     }
 
+    public static void Json() throws IOException {
+        // Créer le json
+        String iconPath = GetIconAndSave();
+
+
+
+
+        try{
+            new File("Db").mkdirs();
+
+        }catch (Exception e){
+            e.printStackTrace();
+            return ;
+        }
+
+    }
 }
 
